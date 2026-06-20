91c0c3beff4367debbe5f4fc77d1b8f283d45447
Sreenidhi-G2 <sreenidhig2005@gmail.com>
2026-06-20 12:32:13 +0530

    "HealthCheckerService Completed With 12k req/sec"

---
+++ src/main/java/com/sreenidhi/load/balancer/service/healthChecker/HealthCheckService.java
@@ -0,0 +1,59 @@
+package  com.sreenidhi.load.balancer.service.healthChecker;
+import com.sreenidhi.load.balancer.service.registry.ServiceRegistry;
+import com.sreenidhi.load.balancer.service.model.BackendServer;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.time.Duration;
+
+@Component
+public class HealthCheckService {
+
+    private final ServiceRegistry serviceRegistry;
+    private final WebClient webClient;
+    int count = 0;
+    public HealthCheckService(ServiceRegistry serviceRegistry , WebClient webClient) {
+        this.serviceRegistry = serviceRegistry;
+        this.webClient = webClient;
+    }
+
+    private void checkServer(BackendServer server) {
+
+        webClient.get()
+                .uri(server.getBaseUrl() + "/api/test")
+                .retrieve()
+                .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(2))
+                .doOnSuccess(response -> {
+                    count = count + 1;
+                    server.setHealthy(true);
+                })
+                .doOnError(error -> {
+                    server.setHealthy(false);
+                })
+                .subscribe();
+
+
+    }
+
+
+
+    @Scheduled(fixedRate = 5000)
+    public void checkServers() {
+
+        serviceRegistry.getAllServers()
+                .forEach(this::checkServer);
+
+//        System.out.println(
+//                "Healthy servers: " +
+//                        serviceRegistry.getHealthyServers().size());
+    }
+
+
+
+
+
+
+}
+
+
