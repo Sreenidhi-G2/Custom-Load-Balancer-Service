793b998e1328d092fde8856e30a6586b47fe89a2
Sreenidhi-G2 <sreenidhig2005@gmail.com>
2026-06-22 22:19:46 +0530

    "Metrics Added with k6 and prometheus"

---
+++ src/main/java/com/sreenidhi/load/balancer/service/controller/MetricsController.java
@@ -0,0 +1,51 @@
+package com.sreenidhi.load.balancer.service.controller;
+
+import com.sreenidhi.load.balancer.service.model.BackendServer;
+import com.sreenidhi.load.balancer.service.registry.ServiceRegistry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+public class MetricsController {
+
+    private final ServiceRegistry serviceRegistry;
+
+    public MetricsController(ServiceRegistry serviceRegistry) {
+        this.serviceRegistry = serviceRegistry;
+    }
+
+    @GetMapping("/metrics")
+    public List<Map<String, Object>> metrics() {
+
+        List<Map<String, Object>> response = new ArrayList<>();
+
+        for (BackendServer server :
+                serviceRegistry.getAllServers()) {
+
+            Map<String, Object> serverMetrics =
+                    new HashMap<>();
+
+            serverMetrics.put(
+                    "serverId",
+                    server.getId());
+
+            serverMetrics.put(
+                    "healthy",
+                    server.isHealthy());
+
+            serverMetrics.put(
+                    "activeConnections",
+                    server.getActiveConnections().get());
+
+            serverMetrics.put(
+                    "totalRequests",
+                    server.getTotalRequests().get());
+
+            response.add(serverMetrics);
+        }
+
+        return response;
+    }
+}
