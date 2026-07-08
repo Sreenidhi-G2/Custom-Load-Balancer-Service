4c31786a3dad9f124e8360f57e64554657be0c91
Sreenidhi-G2 <sreenidhig2005@gmail.com>
2026-07-08 12:02:54 +0530

    "Project Completed"

--- src/main/java/com/sreenidhi/load/balancer/service/LoadBalancerServiceApplication.java
+++ src/main/java/com/sreenidhi/load/balancer/service/LoadBalancerServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import reactor.core.publisher.Hooks;
 
 @EnableScheduling
 @SpringBootApplication
 public class LoadBalancerServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LoadBalancerServiceApplication.class, args);
 	}
 
